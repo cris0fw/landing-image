8c31b78665ddd5aa47eff314f7dd44f07ccb48bd
cris0fw <crisludue5@gmail.com>
2025-04-19 00:29:33 -0300

    ultimo detalle CardOurTeam

--- src/components/CardOurTeam.jsx
+++ src/components/CardOurTeam.jsx
@@ -7,19 +7,21 @@ const CardOurTeam = ({ image, name, skill, delay }) => {
       data-aos="fade-up"
       data-aos-duration={800}
       data-aos-delay={delay}
-      className="w-full max-[310px]"
+      className="w-full group max-[310px] relative"
     >
-      <img
-        src={`/landing-image/${image}`}
-        className="w-full"
-        alt="Imagen of the our tema"
-      />
+      <div className="relative w-full">
+        <img
+          src={`/landing-image/${image}`}
+          className="w-full"
+          alt="Imagen of the our team"
+        />
 
-      {/* Barra social oculta que aparece desde arriba */}
-      <div className="absolute top-[57%] left-0 w-full flex justify-center items-center gap-5 bg-blue-500 h-[50px] translate-y-[100%] opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 z-10">
-        <FaFacebookF className="text-white cursor-pointer" />
-        <FaTwitter className="text-white cursor-pointer" />
-        <FaInstagram className="text-white cursor-pointer" />
+        {/* Barra social superpuesta abajo de la imagen */}
+        <div className="absolute bottom-0 left-0 w-full flex justify-center items-center gap-5 bg-blue-500 h-[50px] translate-y-full opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 z-10">
+          <FaFacebookF className="text-white cursor-pointer" />
+          <FaTwitter className="text-white cursor-pointer" />
+          <FaInstagram className="text-white cursor-pointer" />
+        </div>
       </div>
 
       <div className="px-2 py-7">
